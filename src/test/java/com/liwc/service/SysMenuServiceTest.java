a6d9b9861140dfa4c0eb81855ef82698e99bc09f
licerlee <licerlee@gmail.com>
2018-03-14 09:37:45 +0800

    test

--- src/test/java/com/liwc/service/SysMenuServiceTest.java
+++ src/test/java/com/liwc/service/SysMenuServiceTest.java
@@ -6,14 +6,22 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.liwc.mapper.SysMenuMapper;
 import com.liwc.model.SysMenu;
 
+//
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="classpath*:spring-*.xml")
+
+// 注入事物管理器
+@TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
+@Transactional
 public class SysMenuServiceTest {
 
 	
@@ -39,7 +47,17 @@ public class SysMenuServiceTest {
 	}
 
 	@Test
+	@Rollback() // 默认回滚
 	public void testSave() {
+		
+		SysMenu sm = new SysMenu();
+		sm.setmName("Echarts");
+		sm.setmLevel(2);
+		sm.setmParent(4);
+		sm.setmOrder(3);
+		
+		mapper.insert(sm);
+		
 		
 	}
 
